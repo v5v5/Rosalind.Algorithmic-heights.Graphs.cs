ba6bdea5c9a9f83731ac2d717522914be7f5c2fd
v5v5 <v5@hotbox.ru>
2015-08-26 15:44:04 +0300

    upgrade

---
+++ _Shared/Algorithms.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Shared
+{
+    class Algorithms
+    {
+
+        static public int[] GetVerticesDegree(UndirectedGraph g)
+        {
+            int[] res = new int[g.CountVertices];
+
+            for (int v = 0; v < g.CountVertices; v++)
+            {
+                res[v] = g.Degree(v);
+            }
+
+            return res;
+        }
+    }
+}
