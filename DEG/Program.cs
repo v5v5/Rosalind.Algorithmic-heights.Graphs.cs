0f0c8003ae4554ea872db025f8b15ac9ad33d1b2
v5v5 <v5@hotbox.ru>
2015-08-26 14:34:02 +0300

    start

---
+++ DEG/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "rosalind_deg";
+            UndirectedGraph g = new UndirectedGraph(file + ".txt");
+
+            int[] res = new int[g.CountVertices];
+
+            for (int v = 0; v < g.CountVertices; v++)
+            {
+                res[v] = g.Degree(v);
+                //Console.Write("{0} ", res[v]);
+            }
+
+            Utils.PrintArrayToFile(res, file + ".out.txt");
+
+            Console.Write("Finish.");
+            Console.ReadKey();
+        }
+    }
+}
